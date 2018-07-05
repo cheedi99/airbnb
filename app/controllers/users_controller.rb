27599cf145b66ad1b3dc1b0e2ce52c1a07e42b87
cheedi99 <cheedi9corp@gmail.com>
2018-07-05 17:14:43 +0800

    Done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < Clearance::UsersController
+	def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+
+  def user_from_params
+    email = user_params.delete(:email)
+    first_name = user_params.delete(:first_name)
+    last_name = user_params.delete(:last_name)
+    password = user_params.delete(:password)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.first_name = first_name
+      user.last_name = last_name
+      user.password = password
+    end
+  end
+
+end	
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+	def index
+		# render "welcome/index.html.erb"
+
+	end	
+end
